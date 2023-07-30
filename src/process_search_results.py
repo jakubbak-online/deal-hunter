049b42338f25947cbd4394d860514e9193dfa143
CodenameDorian <jakubbak777@gmail.com>
2023-07-30 19:48:34 +0200

    forgot to add unversioned files

---
+++ src/process_search_results.py
@@ -0,0 +1,26 @@
+import csv
+
+
+def remove_duplicates(file_name="./data/search_offers_data.csv"):
+
+    with open(file_name, 'r+', newline="", encoding="utf-8") as f:
+        csv_read = list(csv.reader(f, delimiter=","))
+        # print(csv_read)
+
+        csv_set = set()
+
+        for line in csv_read:
+            # print(line)
+            csv_set.add(tuple(line))
+
+        csv_write = csv.writer(f, delimiter=',')
+        f.truncate(0)
+
+        for line in csv_set:
+            csv_write.writerow(line)
+        # print((f.readline().split(sep=",")))
+        # csv_list = set(csv.reader(f, delimiter=","))
+        # print(csv_list)
+
+
+remove_duplicates()
