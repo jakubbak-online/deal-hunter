a0afe1e0be886c87a3403734c80a39b6fc20045a
jakubbak-online <jakubbak777@gmail.com>
2023-08-22 11:21:51 +0200

    Made config.py more readable

--- src/config.py
+++ src/config.py
@@ -1,3 +1,3 @@
 # FILL IN YOUR API_KEY AND USER_ID
-params = {"API_KEY": None, "USER_ID": None}
+params = {"API_KEY": "REPLACE THIS (keep quotations)", "USER_ID": "REPLACE THIS (keep quotations)"}
 search_info_location = ".\data\data_long.csv"
