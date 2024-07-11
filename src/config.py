ec2401ec628d43f888eb2d81339c022d30aef932
pacjo <kamil.kroliczek2004@gmail.com>
2024-07-11 19:28:17 +0200

    Fixed file locations when running from outside src, made search_info_location a bit more platform-independent

--- src/config.py
+++ src/config.py
@@ -1,3 +1,17 @@
+import os
+
 # FILL IN YOUR API_KEY AND USER_ID
-params = {"API_KEY": "REPLACE THIS (keep quotations)", "USER_ID": "REPLACE THIS (keep quotations)"}
-search_info_location = ".\data\data_long.csv"
+params = {
+    "API_KEY": "REPLACE THIS (keep quotations)",
+    "USER_ID": "REPLACE THIS (keep quotations)"
+}
+
+main_file_location = os.path.dirname(__file__)
+
+# By default, the file is expected at "./data/data_long.csv".
+# If you need to change the location, modify the path providing
+# directories as sequential arguments.
+#
+# Example for a different location:
+# search_info_location = os.path.join(main_file_location, "my_data", "files", "data.csv")  # ./mydata/files/data.csv
+search_info_location = os.path.join(main_file_location, "data", "data_long.csv")
